b2767d765dcfc90151afc0158079bc9e9ced9fc4
xiaocaifly <rqhua_fly@163.com>
2018-06-14 17:24:32 +0800

    commit

---
+++ app/src/main/java/com/rqhua/demo/defineview/MyLinearLayoutManager.java
@@ -0,0 +1,15 @@
+package com.rqhua.demo.defineview;
+
+import android.content.Context;
+import android.support.v7.widget.LinearLayoutManager;
+
+/**
+ * Created by Administrator on 2018/6/5.
+ */
+
+public class MyLinearLayoutManager extends LinearLayoutManager {
+
+    public MyLinearLayoutManager(Context context, int orientation, boolean reverseLayout) {
+        super(context, orientation, reverseLayout);
+    }
+}
